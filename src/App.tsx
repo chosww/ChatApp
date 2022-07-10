0c61e566837b99cc089af4fae91d7c090d104a8b
Heon Lee <leedylan7989@gmail.com>
2022-07-10 15:00:28 -0400

    Added button role and tabindex

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { registerRootComponent } from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './Button';
 
 class App extends Component {
   render() {
     return (
+    
       <View style={styles.container}>
-        <Text>Open D up App.tsx to start working on your app!</Text>
+        {/* <Text>Open D up App.tsx to start working on your app!</Text> */}
+        <Button buttonText={"Click Me!"} buttonNumber={2} buttonClass="test1"></Button>
         <StatusBar style="auto" />
       </View>
     )
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-//import "./Button.css";
+import "./Button.css";
 
 type ButtonProp = {buttonText : string, buttonNumber : number, buttonClass : string}
 
 class Button extends Component<ButtonProp, {}> {
     handleOnClick = () => {
         for(let i = this.props.buttonNumber; i > 0; i--){
-            console.log("TESTETETETTESTETET")
+            console.log("Test button clicked!")
         }
     }
 
     render() {
         return (
-            <div onClick={this.handleOnClick} className={this.props.buttonClass}>{this.props.buttonText}</div>
+            <div onClick={this.handleOnClick} className={this.props.buttonClass} role="button" tabIndex={0}>{this.props.buttonText}</div>
         )
     }
 }
 
-export default Button
+
+export default Button;
