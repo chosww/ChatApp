74f693f1a65051d871abe3fb8d19a20ad3abf12c
Heon Lee <leedylan7989@gmail.com>
2022-07-10 20:06:01 -0400

    Button works for space and enter

--- src/Button.tsx
+++ src/Button.tsx
@@ -4,15 +4,33 @@ import "./Button.css";
 type ButtonProp = {buttonText : string, buttonNumber : number, buttonClass : string}
 
 class Button extends Component<ButtonProp, {}> {
+
+    buttonAction = () => {
+        console.log("Test button clicked!")
+
+    }
+
     handleOnClick = () => {
         for(let i = this.props.buttonNumber; i > 0; i--){
-            console.log("Test button clicked!")
+            this.buttonAction()
+        }
+    }
+
+    handleKeyDown = (e) => {
+        if(e.key === 'Enter' || e.key === " " || e.code === 13 || e.code === 32) {
+            this.buttonAction()
         }
     }
 
     render() {
         return (
-            <div onClick={this.handleOnClick} className={this.props.buttonClass} role="button" tabIndex={0}>{this.props.buttonText}</div>
+            <div onClick={
+                this.handleOnClick} 
+                className={this.props.buttonClass} 
+                role="button" 
+                tabIndex={0}
+                onKeyDown={this.handleKeyDown}>
+                    {this.props.buttonText}</div>
         )
     }
 }
